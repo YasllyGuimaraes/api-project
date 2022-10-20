a350ffb50763dbef9d774cfad2ff40c8400fb1d0
Yaslly <yaslly.guimaraes@hotmail.com>
2022-10-20 16:18:36 -0300

    Add endpoint documentation.

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -13,6 +13,22 @@ router = APIRouter(
 @router.post('', response_model=ResponsePredict)
 def predict(item: InputPredict):
 
+    """
+    Este endpoint retorna se a pessoa vai ao college ou não de acordo com os dados fornecidos.
+ 
+    **Exemplo:**
+    ```
+    {
+        "type_school": "Academic",
+        "school_accreditation": A,
+        "interest": "Less Interested",
+        "gender": "Male",
+        "residence": "Urban",
+        "parent_was_in_college": false
+    }
+    ```
+    """
+
     will_go_to_college = perform_prediction(item)
     return {
         'will_go_to_college': will_go_to_college
